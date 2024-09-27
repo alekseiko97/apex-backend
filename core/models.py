4a606af78bc7aa69ce6143d86a0d4cecf24eae75
Aleksei Moskaljuk <alekseiko@gmail.com>
2024-09-27 14:29:06 +0200

    Register modes in admin.py

--- core/models.py
+++ core/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Signal to create a UserProfile when a User is created
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -13,11 +12,12 @@ class Organization(models.Model):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE, null=True)
 
     class Meta:
         db_table = 'user_profile'
 
+# Signal to create a UserProfile when a User is created
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
